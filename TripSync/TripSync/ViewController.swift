26a6546332feb740b56164c8c7bc45f6c14d2428
Arpit Garg <arpit1623.be23@chitkara.edu.in>
2025-11-17 10:38:30 +0530

    Initial Project Template

---
+++ TripSync/TripSync/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripSync
+//
+//  Created by Arpit Garg on 14/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
